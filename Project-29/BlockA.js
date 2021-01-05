4e3e7e05c9ccc922b35f490205953b65d5aa95c5
RionaGurung <73350990+RionaGurung@users.noreply.github.com>
2021-01-05 21:10:20 +0530

    Add files via upload

---
+++ Project-29/BlockA.js
@@ -0,0 +1,52 @@
+/*class BlockA extends BaseClass{
+
+  constructor(x, y, width, height){
+
+    super(x,y,width,height);
+ }
+ 
+  display(){
+
+    super.display();
+    fill("#0000CD");
+    stroke("white");
+  }
+}*/
+
+class BlockA{
+
+    constructor(x, y, width, height) {
+
+        var options = {
+
+          'restitution':0.8,
+          'friction':1.0,
+          'density':1.0
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+    
+        World.add(world, this.body);
+
+      }
+
+      display(){
+
+       
+          var angle = this.body.angle;  
+
+          push();
+
+          translate(this.body.position.x, this.body.position.y);
+          rotate(angle);
+          rectMode(CENTER);
+          fill("#0000CD");
+          stroke("white");
+          strokeWeight(2);
+          rect(0, 0, this.width, this.height);
+
+          pop();
+      }
+}
---
+++ Project-29/BlockB.js
@@ -0,0 +1,46 @@
+/*class BlockB extends BaseClass{
+
+  constructor(x, y, width, height){
+
+    super(x,y,width,height);
+ }
+ 
+  display(){
+
+    super.display();
+    fill("#00BFFF");
+    stroke("white");
+  }
+}*/
+class BlockB{
+
+    constructor(x, y, width, height) {
+
+        var options = {
+
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+    
+        World.add(world, this.body);
+      }
+
+      display(){
+
+        push();
+
+        translate(this.body.position.x, this.body.position.y);
+        rectMode(CENTER);
+        fill("#00BFFF");
+        stroke("white");
+        strokeWeight(2);
+        rect(0, 0, this.width, this.height);
+
+        pop();
+      }
+}
